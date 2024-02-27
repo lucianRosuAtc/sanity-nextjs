31b39f0907ded2f6d9cf1e55a5726bb2cc2f9229
Lucian Rosu <lucian.rosu@adopstar.com>
2024-02-27 14:48:32 +0000

    Update fonts, metadata, and tailwind config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+      {
+        protocol: "https",
+        hostname: "https://unsplash.com/s/photos/free",
+      }
+    ],
+  },
+};
 
 export default nextConfig;
