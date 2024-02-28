d9838755f4103f6cb2de08526d3fc4a1f95b1250
Lucian Rosu <lucian.rosu@adopstar.com>
2024-02-28 09:10:09 +0000

    Update Navbar component styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,9 +5,9 @@ import { ModeToggle } from "./ModeToggle";
 export default function Navbar() {
   return (
     <nav className="w-full py-5 bg-black/20 ">
-      <div className="mx-auto flex justify-between items-center max-w-3xl">
+      <div className="mx-auto flex justify-between items-center max-w-4xl px-8">
         <Link href="/">
-          <h1 className="text-3xl md:text-6xl font-semibold">Logo <span className="text-primary">Here</span></h1>
+          <h1 className="text-3xl md:text-5xl font-semibold">Logo <span className="text-primary">Here</span></h1>
         </Link>
       <ModeToggle />
       </div>
