379528a673c8b3e196e321ae4d349b4ccf8efdde
Lucian Rosu <lucian.rosu@adopstar.com>
2024-03-15 16:48:11 +0000

    Update Navbar logo text

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     <nav className="w-full py-5 bg-black/50 ">
       <div className="mx-auto flex justify-between items-center max-w-4xl px-8">
         <Link href="/">
-          <h1 className="text-3xl md:text-5xl font-semibold">Logo <span className="text-primary">Here</span></h1>
+          <h1 className="text-3xl md:text-5xl font-semibold">Luc<span className="text-primary">ian</span>  D<span className="text-primary">ev</span></h1>
         </Link>
       <ModeToggle />
       </div>
